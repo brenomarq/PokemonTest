70346b46d3284acf3d480e7123d08c0f18770a3f
Breno Marques <brenomarques516@gmail.com>
2026-03-04 08:09:14 -0300

    Initial Commit

---
+++ PokemonTest/PokemonTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  PokemonTestApp.swift
+//  PokemonTest
+//
+//  Created by Breno Marques on 04/03/26.
+//
+
+import SwiftUI
+
+@main
+struct PokemonTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
